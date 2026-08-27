355988b67adfd9f5fcabac51978cd88ec02759b9
Duduzile <duduziiiley@gmail.com>
2026-08-27 12:37:39 +0200

    added my pet details on Javascript-Day3.js using functions

---
+++ Module 6- Javascript/Module6-Day3/Javascript-Day3.js
@@ -0,0 +1,22 @@
+let MyPet={
+    Name: "Rosie",
+    Age : 2,
+    Gender : "Female",
+    Breed: "Rottweiler",
+    Coat : "Black and brown",
+    Immunization: "Yes"
+}
+function displayMyPet(MyPet){
+
+    let FoodCost=700;
+
+console.log('============PET DETAILS==============')
+console.log('Name: ' + MyPet.Name);
+console.log('Age: '+ MyPet.Age);
+console.log('Gender: '+ MyPet.Gender);
+console.log('Breed: '+ MyPet.Breed);
+console.log('Coat: ' + MyPet.Coat);
+console.log('Immunization: ' + MyPet.Immunization);
+console.log('Food Cost: ' + "-R" + FoodCost);
+}
+displayMyPet(MyPet);

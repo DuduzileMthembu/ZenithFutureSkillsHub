431b13457e64bb265b21440f241b5183e1021cee
Duduzile <duduziiiley@gmail.com>
2026-08-29 11:35:45 +0200

    Added structure to my study planner for readability

--- Module 6- Javascript/Module6-Assessment/Javascript-Final-Assessment.js
+++ Module 6- Javascript/Module6-Assessment/Javascript-Final-Assessment.js
@@ -1,8 +1,8 @@
 let today = new Date();
 
 console.log("====== PERSONAL STUDY PLANNER ======");
-console.log("STUDY PLANNER :" + today);
-
+console.log("STUDY PLANNER :" + today.toLocaleDateString());
+console.log(' ');
 let Subjects = new Set();
 
 let studySubjects = [
@@ -26,13 +26,18 @@ let studySubjects = [
 
 
 for (let i = 0; i < studySubjects.length; i++) {
-    Subjects.add(studySubjects[i].name);
+    Subjects.add(studySubjects.name);
 }
 console.log('Subjects:');
 for(let subject of Subjects){
-
-    console.log(Subjects);
+ console.log('Javascript');
+ console.log('HTML');
+ console.log('PYTHON');
+ console.log('CSS');
 }
+ console.log(' ')
+
+
 
 let schedule = new Map();
 schedule.set("Monday", "JavaScript");
@@ -60,41 +65,47 @@ function calculateTotalHours(subjects) {
 console.log("Subject Details:");
 for (let i = 0; i < studySubjects.length; i++) {
     console.log(studySubjects[i].name + " -> hours: " + studySubjects[i].hours + " -> " + studySubjects[i].difficulty);
+  
 }
-
+  console.log(' ');
 
 
 console.log("Schedule:");
+    
 for (let [day, subject] of schedule) {
     console.log(day + " -> " + subject);
+    
 }
-
+console.log(' ');
 
 
 function displayDeadlines() {
     console.log("Deadlines:");
     for (let [subject, date] of deadlines) {
-        console.log(subject + " -> " + date);
+        console.log(subject + " -> " + date.toLocaleDateString());
     }
-    
+    console.log(' ');
 }
 
 function checkStudyStatus(subjects) {
     for (let i = 0; i < subjects.length; i++) {
         let name = subjects[i].name;
         let hours = subjects[i].hours;
-
+        
         if (hours > 6) {
             console.log("Good study progress for " + name);
         } else {
-            console.log("Needs more study time for " + name);
+            console.log("Needs more study time for " + name);   
         }
+        
     }
 }
 
 
 let total = calculateTotalHours(studySubjects);
 console.log("Total Study Hours: " + total);
+console.log(' ');
 
 displayDeadlines();
+
 checkStudyStatus(studySubjects);

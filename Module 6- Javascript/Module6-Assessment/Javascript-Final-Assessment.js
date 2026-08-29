cef9752649360f690c1d471f9b0b70fec788e92f
Duduzile <duduziiiley@gmail.com>
2026-08-29 11:07:57 +0200

    Added subjects, dates and times to my study planner

---
+++ Module 6- Javascript/Module6-Assessment/Javascript-Final-Assessment.js
@@ -0,0 +1,100 @@
+let today = new Date();
+
+console.log("====== PERSONAL STUDY PLANNER ======");
+console.log("STUDY PLANNER :" + today);
+
+let Subjects = new Set();
+
+let studySubjects = [
+    { name: "JavaScript", 
+        hours: 6, 
+        difficulty: "medium" 
+    },
+    { name: "HTML", 
+        hours: 4, 
+        difficulty: "easy" 
+    },
+    { name: "PYTHON", 
+        hours: 8, 
+        difficulty: "hard" },
+    { name: "CSS",
+         hours: 6, 
+         difficulty: "medium" 
+    }
+];
+
+
+
+for (let i = 0; i < studySubjects.length; i++) {
+    Subjects.add(studySubjects[i].name);
+}
+console.log('Subjects:');
+for(let subject of Subjects){
+
+    console.log(Subjects);
+}
+
+let schedule = new Map();
+schedule.set("Monday", "JavaScript");
+schedule.set("Tuesday", "CSS");
+schedule.set("Wednesday", "Python");
+schedule.set("Thursday", "HTML");
+schedule.set("Friday", "JavaScript");
+
+
+let deadlines = new Map();
+deadlines.set("JavaScript", new Date("2026-09-10"));
+deadlines.set("HTML", new Date("2026-09-15"));
+deadlines.set("PYTHON", new Date("2026-09-20"));
+deadlines.set("CSS", new Date("2026-09-25"));
+
+function calculateTotalHours(subjects) {
+    let total = 0;
+    for (let i = 0; i < subjects.length; i++) {
+        total = total + subjects[i].hours; // 
+    }
+    return total;
+}
+
+
+console.log("Subject Details:");
+for (let i = 0; i < studySubjects.length; i++) {
+    console.log(studySubjects[i].name + " -> hours: " + studySubjects[i].hours + " -> " + studySubjects[i].difficulty);
+}
+
+
+
+console.log("Schedule:");
+for (let [day, subject] of schedule) {
+    console.log(day + " -> " + subject);
+}
+
+
+
+function displayDeadlines() {
+    console.log("Deadlines:");
+    for (let [subject, date] of deadlines) {
+        console.log(subject + " -> " + date);
+    }
+    
+}
+
+function checkStudyStatus(subjects) {
+    for (let i = 0; i < subjects.length; i++) {
+        let name = subjects[i].name;
+        let hours = subjects[i].hours;
+
+        if (hours > 6) {
+            console.log("Good study progress for " + name);
+        } else {
+            console.log("Needs more study time for " + name);
+        }
+    }
+}
+
+
+let total = calculateTotalHours(studySubjects);
+console.log("Total Study Hours: " + total);
+
+displayDeadlines();
+checkStudyStatus(studySubjects);

885751905cd54a886828fcfb82ea174442280ecf
Duduzile <duduziiiley@gmail.com>
2026-09-11 12:19:25 +0200

    Àdded a few phrases to proffessionalise the countdown tool

--- Module 7- Python/Python-week2/Python-Mini-project3/Mini-Project3.py
+++ Module 7- Python/Python-week2/Python-Mini-project3/Mini-Project3.py
@@ -3,11 +3,17 @@
 import time
 from datetime import datetime
 
+print('==========20 Seconds Core Workout=========')
+print('Ready?!')
+print('Set!')
+print('GO!!')
+
 def countdown(seconds):
     while seconds > 0 :
         print("Time Remaining : ", seconds)
         time.sleep(1)
         seconds -= 1
+    print("WELL DONE!!")
     print("Workout Complete!")
 
 print("Today's Date : ", datetime.now())
